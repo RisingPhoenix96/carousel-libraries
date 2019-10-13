c88d2ee34bdd65ced7bed3993a7a0fce2cb15576
Lamé Moatshe <lame@castlegateit.co.uk>
2019-10-13 23:47:50 +0100

    barebones centered carousel

--- owl-carousel-2/index.php
+++ owl-carousel-2/index.php
@@ -13,7 +13,57 @@ include "../includes/header.php"
 
 <div class="page-wrap">
 
-    <div class="owl-carousel-wrap owl-carousel owl-theme">
+    <h2 class="carousel-type-title">Standard Carousel</h2>
+
+    <div class="carousel standard-carousel-wrap owl-carousel owl-theme">
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=1"></img>
+            <p class="title">Title 1</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=2"></img>
+            <p class="title">Title 2</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=3"></img>
+            <p class="title">Title 3</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=4"></img>
+            <p class="title">Title 4</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=5"></img>
+            <p class="title">Title 5</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image" src="https://picsum.photos/300/200?random=6"></img>
+            <p class="title">Title 6</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+    </div>
+
+    <h2 class="carousel-type-title">Centered Carousel</h2>
+
+    <div class="carousel centered-carousel-wrap owl-carousel owl-theme">
 
         <div class="slide">
             <img class="image" src="https://picsum.photos/300/200?random=1"></img>
--- src/js/owl-slide.js
+++ src/js/owl-slide.js
@@ -1,8 +1,40 @@
 $(function () {
 
-    $('.owl-carousel-wrap').owlCarousel({
+    $('.standard-carousel-wrap').owlCarousel({
         loop: true,
         margin: 10,
+        nav: true,
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+    });
+
+    $('.centered-carousel-wrap').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        center: true,
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
     });
 
 });
