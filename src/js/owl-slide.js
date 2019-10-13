31a29a88b30363f2fb236e8bf4adb67dd8b2f3f2
Lamé Moatshe <lame@castlegateit.co.uk>
2019-10-14 00:06:10 +0100

    centered carousel buttons

--- src/js/owl-slide.js
+++ src/js/owl-slide.js
@@ -1,5 +1,7 @@
 $(function () {
 
+    // Standard carousel
+
     $('.standard-carousel-wrap').owlCarousel({
         loop: true,
         margin: 10,
@@ -18,11 +20,14 @@ $(function () {
         }
     });
 
+    // Centered carousel
+
     $('.centered-carousel-wrap').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         center: true,
+        navText: ["<button class='centered-carousel-prev'>Prev</button>", "<button class='centered-carousel-next'>Next</button>"],
 
         responsive: {
             0: {
@@ -30,9 +35,6 @@ $(function () {
             },
             640: {
                 items: 2
-            },
-            960: {
-                items: 3
             }
         }
     });
