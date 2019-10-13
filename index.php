a63cdd388c1309f7f98fe1a1df20e519ca6c373f
Lamé Moatshe <lame@castlegateit.co.uk>
2019-10-13 20:20:58 +0100

    adding header and footer as includes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php" ?>
+
+<div class="page-wrap">
+    <h1>Carousel Libraries</h1>
+    <ul>
+        <li><a href="/owl-carousel-2/">Owl Carousel 2</a></li>
+    </ul>
+</div>
+
+<?php include "includes/footer.php" ?>
