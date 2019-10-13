31d973362f1a04ee724fb4c148f38d784fc7e882
Lamé Moatshe <lame@castlegateit.co.uk>
2019-10-13 20:41:31 +0100

    setup

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,9 @@
 <html lang="en">
 
 <head>
-    <title>Templates</title>
+    <title>jQuery Carousel Libraries <?php if (isset($page_info['title'])) {
+                                            echo "| " . $page_info['title'];
+                                        } ?></title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- owl-carousel-2/index.php
+++ owl-carousel-2/index.php
@@ -1,3 +1,11 @@
-<?php include "../includes/header.php" ?>
+<?php
+
+$page_info = array(
+    'title' => 'Owl Carousel 2',
+);
+
+include "../includes/header.php"
+
+?>
 
 <?php include "../includes/footer.php" ?>
