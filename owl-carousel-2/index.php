20672fbfc6d895c0eb4331ea9604e476d45757f0
Lamé Moatshe <lame@castlegateit.co.uk>
2019-10-14 19:37:52 +0100

    lazy loading

--- owl-carousel-2/index.php
+++ owl-carousel-2/index.php
@@ -109,6 +109,54 @@ include "../includes/header.php"
 
     </div>
 
+    <h2 class="carousel-type-title">Lazy Carousel</h2>
+
+    <div class="carousel lazy-carousel-wrap -side-arrows owl-carousel owl-theme">
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=1"></img>
+            <p class="title">Title 1</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=2"></img>
+            <p class="title">Title 2</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=3"></img>
+            <p class="title">Title 3</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=4"></img>
+            <p class="title">Title 4</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=5"></img>
+            <p class="title">Title 5</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+        <div class="slide">
+            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=6"></img>
+            <p class="title">Title 6</p>
+            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
+            <a href="#" class="button-link">Read More</a>
+        </div>
+
+    </div>
+
 </div>
 
 <?php include "../includes/footer.php" ?>
--- src/js/owl-slide.js
+++ src/js/owl-slide.js
@@ -39,4 +39,28 @@ $(function () {
         }
     });
 
+    // Lazy carousel
+
+    $('.lazy-carousel-wrap').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        center: false,
+        lazyLoad: true,
+        //lazyLoadEager: 1,
+        navText: ["<button class='carousel-control centered-carousel-prev prev'>&#9664;</button>", "<button class='carousel-control centered-carousel-next next'>&#9654;</button>"],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+    });
+
 });
