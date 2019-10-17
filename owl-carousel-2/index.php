02f6a3f6453f781a616073e27941bab4e9c03f73
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-17 11:09:41 +0100

    sorting files

--- owl-carousel-2/index.php
+++ owl-carousel-2/index.php
@@ -15,195 +15,19 @@ include "../includes/header.php"
 
     <h2 class="carousel-type-title">Standard Slider</h2>
 
-    <div class="carousel standard-slider-wrap -side-arrows owl-carousel owl-theme">
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=1"></img>
-            <p class="title">Title 1</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=2"></img>
-            <p class="title">Title 2</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=3"></img>
-            <p class="title">Title 3</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=4"></img>
-            <p class="title">Title 4</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=5"></img>
-            <p class="title">Title 5</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=6"></img>
-            <p class="title">Title 6</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-    </div>
+    <?php include "./parts/standard_slider.php"; ?>
 
     <h2 class="carousel-type-title">Standard Carousel</h2>
 
-    <div class="carousel standard-carousel-wrap owl-carousel owl-theme">
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=1"></img>
-            <p class="title">Title 1</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=2"></img>
-            <p class="title">Title 2</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=3"></img>
-            <p class="title">Title 3</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=4"></img>
-            <p class="title">Title 4</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=5"></img>
-            <p class="title">Title 5</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=6"></img>
-            <p class="title">Title 6</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-    </div>
+    <?php include "./parts/standard_carousel.php"; ?>
 
     <h2 class="carousel-type-title">Centered Carousel</h2>
 
-    <div class="carousel centered-carousel-wrap -side-arrows owl-carousel owl-theme">
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=1"></img>
-            <p class="title">Title 1</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=2"></img>
-            <p class="title">Title 2</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=3"></img>
-            <p class="title">Title 3</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=4"></img>
-            <p class="title">Title 4</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=5"></img>
-            <p class="title">Title 5</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image" src="https://picsum.photos/300/200?random=6"></img>
-            <p class="title">Title 6</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-    </div>
+    <?php include "./parts/centered_carousel.php"; ?>
 
     <h2 class="carousel-type-title">Lazy Carousel</h2>
 
-    <div class="carousel lazy-carousel-wrap -side-arrows owl-carousel owl-theme">
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=1"></img>
-            <p class="title">Title 1</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=2"></img>
-            <p class="title">Title 2</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=3"></img>
-            <p class="title">Title 3</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=4"></img>
-            <p class="title">Title 4</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=5"></img>
-            <p class="title">Title 5</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-        <div class="slide">
-            <img class="image owl-lazy" data-src="https://picsum.photos/300/200?random=6"></img>
-            <p class="title">Title 6</p>
-            <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
-            <a href="#" class="button-link">Read More</a>
-        </div>
-
-    </div>
+    <?php include "./parts/lazy_carousel.php"; ?>
 
 </div>
 
--- src/js/owl-slide.js
+++ src/js/owl-slide.js
@@ -73,4 +73,25 @@ $(function () {
         }
     });
 
+    // Animated carousel
+
+    $('.animated-carousel-wrap').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["<button class='carousel-control centered-carousel-prev prev'>&#9664;</button>", "<button class='carousel-control centered-carousel-next next'>&#9654;</button>"],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+    });
+
 });
