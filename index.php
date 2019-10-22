8b0680ea06354fa33a14f33a7c7f115c5c8daa72
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-22 13:11:50 +0100

    home nav

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <h1>Carousel Libraries</h1>
     <ul>
         <li><a href="/owl-carousel-2/">Owl Carousel 2</a></li>
+        <li><a href="#">Tiny Slider</a></li>
+        <li><a href="#">Bootstrap Slider</a></li>
+        <li><a href="#">Slick</a></li>
     </ul>
 </div>
 
