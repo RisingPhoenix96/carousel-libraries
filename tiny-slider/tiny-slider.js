7325c5e5cdcc747525f549979bed7c049e7ab21b
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-22 15:32:27 +0100

    standard carousel

--- tiny-slider/tiny-slider.js
+++ tiny-slider/tiny-slider.js
@@ -16,3 +16,38 @@ $(function () {
     });
 
 });
+
+$(function () {
+
+    // Standard carousel
+    const tnsStandardCarousel = tns({
+
+        container: '.tiny-standard-carousel-wrap',
+        items: 1,
+        loop: true,
+        nav: true,
+        navPosition: 'bottom',
+        mouseDrag: true,
+        arrowKeys: false,
+        touch: true,
+        controls: true,
+        //controlsPosition: 'bottom',
+        //controlsText: ['prev', 'next'],
+        controlsContainer: '.tns-standard-carousel-controls',
+        //autoplayPosition: 'bottom',
+        center: true,
+        //gutter: 5,
+        //edgePadding: 50,
+
+        responsive: {
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+
+    });
+
+});
