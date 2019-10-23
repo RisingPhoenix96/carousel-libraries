308f65f9ad5cf40269e43d7682353654897a72d8
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-23 15:10:59 +0100

    tweaks

--- tiny-slider/index.php
+++ tiny-slider/index.php
@@ -12,42 +12,42 @@ include './parts/tiny_slider_nav.php';
         <div class="carousel -side-arrows tiny-standard-slider-wrap">
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=1"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=1"></img>
                 <p class="title">Title 1</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
             </div>
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=2"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=2"></img>
                 <p class="title">Title 2</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
             </div>
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=3"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=3"></img>
                 <p class="title">Title 3</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
             </div>
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=4"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=4"></img>
                 <p class="title">Title 4</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
             </div>
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=5"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=5"></img>
                 <p class="title">Title 5</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
             </div>
 
             <div class="slide">
-                <img class="image" src="https://picsum.photos/928/200?random=6"></img>
+                <img class="image" src="https://picsum.photos/928/300?random=6"></img>
                 <p class="title">Title 6</p>
                 <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
                 <a href="#" class="button-link">Read More</a>
