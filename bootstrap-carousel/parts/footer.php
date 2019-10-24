27548d909f946f767e0a428adb22196df686a7ce
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-24 12:27:18 +0100

    initial commit

---
+++ bootstrap-carousel/parts/footer.php
@@ -0,0 +1,3 @@
+</div><!-- end of .page-wrap -->
+
+<?php include "../includes/footer.php" ?>
---
+++ bootstrap-carousel/parts/header.php
@@ -0,0 +1,19 @@
+<?php
+
+$page_info = array(
+    'title' => 'Bootstrap Carousel',
+    'body_id' => 'bootstrap-carousel',
+);
+
+include "../includes/header.php";
+
+?>
+
+<script src="/bootstrap-carousel/bootstrap.carousel.js"></script>
+
+<h1 class="banner -owl-carousel-banner">
+    Bootstrap Carousel<br />
+    <a href="/" class="home-link">Front Page</a>
+</h1>
+
+<div class="page-wrap">
