d3a35b18fc8b5e003ffde9592d113d1014392a11
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-24 09:32:15 +0100

    slight home page rework

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 
 <h1 style="margin: 1rem 0; text-align: center;">Carousel Libraries</h1>
 
-<div class="intro-wrapper">
+<!-- <div class="intro-wrapper">
 
     <a href="/owl-carousel-2/">Owl Carousel 2</a></<a>
     <a href="/tiny-slider/">Tiny Slider</a>
     <a href="#">Bootstrap Slider</a>
     <a href="#">Slick</a>
 
+</div> -->
+
+<div class="page-wrap">
+    <ul>
+        <li><a href="/tiny-slider/">Tiny Slider</a></li>
+        <li><a href="#">Bootstrap Slider</a></li>
+        <li><a href="#">Slick</a></li>
+    </ul>
 </div>
 
 <?php include "includes/footer.php" ?>
