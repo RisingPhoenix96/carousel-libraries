7f187285bd88cbe1283c4b8803b4855e008aca33
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-29 14:17:47 +0000

    adding standard carousel

--- slick/standard_carousel.php
+++ slick/standard_carousel.php
@@ -1,51 +1,51 @@
 <?php
 include './parts/header.php';
 
-include './parts/tiny_slider_nav.php';
+include './parts/slick_nav.php';
 ?>
 
 <h2 class="carousel-type-title">Standard Carousel</h2>
 
 <div class="wrapper">
-    <div class="carousel -side-arrows tiny-standard-carousel-wrap">
+    <div class="carousel -side-arrows slick-standard-carousel-wrap">
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=1"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=1"></img>
             <p class="title">Title 1</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=2"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=2"></img>
             <p class="title">Title 2</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=3"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=3"></img>
             <p class="title">Title 3</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=4"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=4"></img>
             <p class="title">Title 4</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=5"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=5"></img>
             <p class="title">Title 5</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image" src="https://picsum.photos/320/200?random=6"></img>
+            <img class="image" src="https://picsum.photos/480/200?random=6"></img>
             <p class="title">Title 6</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
@@ -62,35 +62,34 @@ include './parts/tiny_slider_nav.php';
 <script>
     $(function() {
 
-        // Standard carousel
-        const tnsStandardCarousel = tns({
+        $('.carousel').slick({
 
-            autoplay: true,
-            autoplayButtonOutput: false,
-            autoplayTimeout: 6000,
-            autoplayHoverPause: true,
-            speed: 1200,
-            slideBy: 'page', //number of items to slide = number of slides displayed / number of items in the pager
-            container: '.tiny-standard-carousel-wrap',
-            items: 1,
-            loop: true,
-            nav: true,
-            navPosition: 'bottom',
-            mouseDrag: true,
-            arrowKeys: false,
-            touch: true,
-            controls: true,
-            controlsContainer: '.tns-standard-carousel-controls',
-            center: true,
+            'autoplay': true,
+            'autoplaySpeed': '3500',
+            'infinite': true,
+            'slidesToShow': 3,
+            'slidesToScroll': 1,
+            'dots': true,
+            'speed': 1500,
+            'arrows': true,
+            'prevArrow': $('.controls .prev'),
+            'nextArrow': $('.controls .next'),
 
-            responsive: {
-                640: {
-                    items: 2
+            responsive: [
+
+                {
+                    breakpoint: 640,
+                    settings: {
+                        'slidesToShow': 2,
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        'slidesToShow': 1,
+                    }
                 },
-                960: {
-                    items: 3
-                }
-            }
+
+            ]
 
         });
 
