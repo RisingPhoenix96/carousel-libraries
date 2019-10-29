aa93afeb615212884a3ae06c33751a3e5cbe6a00
Lame Moatshe <lame@castlegateit.co.uk>
2019-10-29 14:48:23 +0000

    adding lazy carousel

--- slick/lazy_carousel.php
+++ slick/lazy_carousel.php
@@ -1,51 +1,51 @@
 <?php
 include './parts/header.php';
 
-include './parts/tiny_slider_nav.php';
+include './parts/slick_nav.php';
 ?>
 
 <h2 class="carousel-type-title">Lazy Carousel</h2>
 
 <div class="wrapper">
-    <div class="carousel -side-arrows tiny-lazy-carousel-wrap">
+    <div class="carousel -side-arrows slick-standard-carousel-wrap">
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=1" data-src="https://picsum.photos/500/200?random=1"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=1"></img>
             <p class="title">Title 1</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=2" data-src="https://picsum.photos/500/200?random=2"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=2"></img>
             <p class="title">Title 2</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=3" data-src="https://picsum.photos/500/200?random=3"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=3"></img>
             <p class="title">Title 3</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=4" data-src="https://picsum.photos/500/200?random=4"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=4"></img>
             <p class="title">Title 4</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=5" data-src="https://picsum.photos/500/200?random=5"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=5"></img>
             <p class="title">Title 5</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=6" data-src="https://picsum.photos/500/200?random=6"></img>
+            <img class="image" data-lazy="https://picsum.photos/480/200?random=6"></img>
             <p class="title">Title 6</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
@@ -53,7 +53,7 @@ include './parts/tiny_slider_nav.php';
 
     </div>
 
-    <div class="controls tns-lazy-carousel-controls">
+    <div class="controls tns-standard-carousel-controls">
         <button class="carousel-control centered-carousel-prev prev">◀</button>
         <button class="carousel-control centered-carousel-next next">▶</button>
     </div>
@@ -62,40 +62,43 @@ include './parts/tiny_slider_nav.php';
 <script>
     $(function() {
 
-        // Lazy carousel
-        const tnsLazyCarousel = tns({
+        $('.carousel').slick({
 
-            autoplay: true,
-            autoplayButtonOutput: false,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-            speed: 1000,
-            container: '.tiny-lazy-carousel-wrap',
-            items: 1,
-            loop: true,
-            nav: true,
-            navPosition: 'bottom',
-            mouseDrag: true,
-            arrowKeys: false,
-            touch: true,
-            controls: true,
-            //controlsPosition: 'bottom',
-            //controlsText: ['prev', 'next'],
-            controlsContainer: '.tns-lazy-carousel-controls',
-            //autoplayPosition: 'bottom',
-            center: true,
-            lazyload: true,
-            //"lazyloadSelector": ".tns-lazy",
+            'autoplay': true,
+            'autoplaySpeed': '3500',
+            'infinite': true,
+            'slidesToShow': 3,
+            'slidesToScroll': 1,
+            'dots': true,
+            'speed': 1500,
+            'arrows': true,
+            'prevArrow': $('.controls .prev'),
+            'nextArrow': $('.controls .next'),
+            'centerMode': true,
+            'centerPadding': '40px',
+            'lazyLoad': 'ondemand',
 
-            responsive: {
-                640: {
-                    items: 2
-                },
-                960: {
-                    items: 1,
-                    edgePadding: 230,
+            responsive: [
+
+                {
+                    breakpoint: 769,
+                    settings: {
+                        'slidesToShow': 2,
+                    }
+                }, {
+                    breakpoint: 641,
+                    settings: {
+                        'slidesToShow': 1,
+                    }
+                }, {
+                    breakpoint: 481,
+                    settings: {
+                        'centerMode': false,
+                        'slidesToShow': 1,
+                    }
                 }
-            }
+
+            ]
 
         });
 
