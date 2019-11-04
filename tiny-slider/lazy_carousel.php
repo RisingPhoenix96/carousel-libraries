fc475661190f6d2e4de8128e7d9804d8dbb360fb
Lame Moatshe <lame@castlegateit.co.uk>
2019-11-04 10:38:12 +0000

    placeholder images for Tiny lazy carousel

--- tiny-slider/lazy_carousel.php
+++ tiny-slider/lazy_carousel.php
@@ -10,42 +10,42 @@ include './parts/tiny_slider_nav.php';
     <div class="carousel -side-arrows tiny-lazy-carousel-wrap">
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=1" data-src="https://picsum.photos/500/200?random=1"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=1" data-src="https://picsum.photos/500/200?random=1"></img>
             <p class="title">Title 1</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=2" data-src="https://picsum.photos/500/200?random=2"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=2" data-src="https://picsum.photos/500/200?random=2"></img>
             <p class="title">Title 2</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=3" data-src="https://picsum.photos/500/200?random=3"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=3" data-src="https://picsum.photos/500/200?random=3"></img>
             <p class="title">Title 3</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=4" data-src="https://picsum.photos/500/200?random=4"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=4" data-src="https://picsum.photos/500/200?random=4"></img>
             <p class="title">Title 4</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=5" data-src="https://picsum.photos/500/200?random=5"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=5" data-src="https://picsum.photos/500/200?random=5"></img>
             <p class="title">Title 5</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
         </div>
 
         <div class="slide">
-            <img class="image tns-lazy-img" src="https://picsum.photos/500/200?random=6" data-src="https://picsum.photos/500/200?random=6"></img>
+            <img class="image tns-lazy-img" src="https://via.placeholder.com/500x200.png?text=6" data-src="https://picsum.photos/500/200?random=6"></img>
             <p class="title">Title 6</p>
             <p class="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur nam asperiores tenetur iste sit. Repellendus voluptatem odio ipsa quisquam ullam quis.</p>
             <a href="#" class="button-link">Read More</a>
